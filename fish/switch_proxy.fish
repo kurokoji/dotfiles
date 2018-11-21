4bfc0b403365229d8131f55d858934bbf536bd6b
kurokoji <shu.kakihana@gmail.com>
2018-11-21 21:56:32 +0900

    feat: add no_proxy

--- fish/switch_proxy.fish
+++ fish/switch_proxy.fish
@@ -5,12 +5,14 @@ set -x http_proxy ''
 set -x https_proxy ''
 set -x ftp_proxy ''
 set -x all_proxy ''
+set -x no_proxy ''
 
 function set_proxy
   set -x http_proxy "http://$proxy"
   set -x https_proxy "http://$proxy"
   set -x ftp_proxy "http://$proxy"
   set -x all_proxy $proxy
+  set -x no_proxy '127.0.0.1,localhost'
 
   git config --global http.proxy "http://$proxy"
   git config --global https.proxy "https://$proxy"
@@ -22,6 +24,7 @@ function unset_proxy
   set -e https_proxy
   set -e ftp_proxy
   set -e all_proxy
+  set -e no_proxy
 
   git config --global --unset http.proxy
   git config --global --unset https.proxy
