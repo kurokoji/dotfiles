005d01466edc733fddf6cee29826ad716adcce58
kurokoji <shu.kakihana@gmail.com>
2021-08-12 22:59:18 +0900

    update

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,6 +2,8 @@ Set-PoshPrompt -Theme powerlevel10k_modern
 
 Set-Item ENV:Path "$ENV:HOME\.cargo\bin;$ENV:Path"
 
+Set-PSReadLineOption -PredictionSource History
+
 # Alias {{{
 
 Set-Alias vim nvim
