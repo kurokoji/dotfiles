4d8923dad0f4c6aa1bb344773a7b29876b1fcb1f
Shu Kakihana <kurokoji@users.noreply.github.com>
2022-06-16 18:54:01 +0900

    update

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -27,7 +27,7 @@ function touch($file) {
 # }}}
 
 # PSfzf
-# Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t'
-# Set-PsFzfOption -PSReadlineChordReverseHistory 'Ctrl+r'
-# Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
-# $env:FZF_DEFAULT_OPTS="--height 50% --layout=reverse"
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t'
+Set-PsFzfOption -PSReadlineChordReverseHistory 'Ctrl+r'
+Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
+$env:FZF_DEFAULT_OPTS="--height 30% --layout=reverse"
