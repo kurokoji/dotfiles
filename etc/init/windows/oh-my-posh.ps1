cbfb5a420ec055a56002a8add1c422158df93ed4
kurokoji <shu.kakihana@gmail.com>
2023-01-15 02:08:13 +0900

    fix: oh-my-poshのインストール方法をアプデ

--- etc/init/windows/oh-my-posh.ps1
+++ etc/init/windows/oh-my-posh.ps1
@@ -1,4 +1,1 @@
-Install-Module posh-git -Scope CurrentUser
 winget install JanDeDobbeleer.OhMyPosh -s winget
-
-Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
