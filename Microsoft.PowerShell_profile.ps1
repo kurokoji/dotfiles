b3a8f47adbeefbcf80ac8c7f18f50a09de9bf86f
Shu Kakihana <kurokoji@users.noreply.github.com>
2023-05-03 23:53:20 +0900

    feat: pwshのthemeを変更

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
 # Set-PoshPrompt -Theme pure
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/emodipt-extend.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/amro.omp.json" | Invoke-Expression
 
 Set-Item ENV:Path "$ENV:HOMEPATH\.cargo\bin;$ENV:Path"
 # Set-Item ENV:Path "~\.cargo\bin;$ENV:Path"
