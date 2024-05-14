4e6258185443173618c89eab86eb1c91db6555ac
Shu Kakihana <shu.kakihana@gmail.com>
2024-05-14 17:24:20 +0900

    fix: lspのsignsを変更

--- config/nvim/toml/lazy/hooks_file/nvim-lspconfig.lua
+++ config/nvim/toml/lazy/hooks_file/nvim-lspconfig.lua
@@ -17,7 +17,7 @@ end
 -- }}}
 
 -- lua_source {{{
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = { Error = "", Warn = "", Hint = "", Info = "" }
 for type, icon in pairs(signs) do
 	local hl = "DiagnosticSign" .. type
 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
