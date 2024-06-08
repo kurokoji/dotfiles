4cf8f77728bdb410094df92f84af9988807977fd
Shu Kakihana <shu.kakihana@gmail.com>
2024-06-08 14:29:11 +0900

    feat: update

--- config/nvim/toml/lazy/hooks_file/lightline.lua
+++ config/nvim/toml/lazy/hooks_file/lightline.lua
@@ -1,5 +1,35 @@
 -- lua_add {{{
-vim.g.lightline = {
-	colorscheme = 'tokyonight',
+local g = vim.g
+
+local mode_map = {
+	n = "N",
+	i = "I",
+	R = "R",
+	v = "V",
+	V = "VL",
+	["<C-v>"] = "VB",
+	c = "C",
+	s = "S",
+	S = "SL",
+	["<C-s>"] = "SB",
+	t = "T",
 }
+
+local separator = {
+	left = "",
+	right = "",
+}
+
+local subseparator = {
+	left = "",
+	right = "",
+}
+
+g.lightline = {
+	colorscheme = "tokyonight",
+	mode_map = mode_map,
+	separator = separator,
+	subseparator = subseparator,
+}
+
 -- }}}
