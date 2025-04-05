6ff597b8c43ff03694b1c2cd8d5b7ffdf96efb6c
Shu Kakihana <shu.kakihana@gmail.com>
2025-04-05 17:42:46 +0900

    feat: update

--- config/nvim/toml/lazy/hooks_file/none-ls.lua
+++ config/nvim/toml/lazy/hooks_file/none-ls.lua
@@ -2,15 +2,17 @@
 local null_ls = require("null-ls")
 
 null_ls.setup({
-	sources = {
-		null_ls.builtins.formatting.rubocop.with({
-			command = "bundle",
-			args = { "exec", "rubocop", "-a", "--server", "-f", "quiet", "--stderr", "--stdin", "$FILENAME" },
-		}),
-		null_ls.builtins.diagnostics.rubocop.with({
-			command = "bundle",
-			args = { "exec", "rubocop", "-f", "json", "--force-exclusion", "--stdin", "$FILENAME" },
-		}),
-	},
+	-- sources = {
+	-- 	null_ls.builtins.formatting.rubocop.with({
+	-- 		command = "bundle",
+	-- 		args = { "exec", "rubocop", "-a", "--server", "-f", "quiet", "--stderr", "--stdin", "$FILENAME" },
+	-- 	}),
+	-- 	null_ls.builtins.diagnostics.rubocop.with({
+	-- 		command = "bundle",
+	-- 		args = { "exec", "rubocop", "-f", "json", "--force-exclusion", "--stdin", "$FILENAME" },
+	-- 	}),
+	-- },
 })
+
+vim.api.nvim_exec_autocmds("User", { pattern = "NoneLsConfigLoaded" })
 -- }}}
