e6e564a2b720a7ecd216244c61d7603c4be5c928
kurokoji <shu.kakihana@gmail.com>
2026-01-18 22:25:32 +0900

    feat: update

--- config/nvim/lua/settings/opts.lua
+++ config/nvim/lua/settings/opts.lua
@@ -77,7 +77,8 @@ vim.api.nvim_create_autocmd("ColorScheme", {
 })
 
 -- Colorscheme (ensure it's installed)
-vim.cmd.colorscheme "melange"
+-- vim.cmd.colorscheme "melange"
+vim.cmd.colorscheme "sakuracream"
 
 local use_neovide = vim.g.neovide ~= nil
 local use_gui = use_neovide or vim.fn.has("gui_running") == 1
